317d78d0d8ac4ff32f1c93160c2b70b603d1619f
Andrey Cherkashin <tosmsandreyka@gmail.com>
2016-04-04 18:07:31 +0300

    six commit

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -6,8 +6,17 @@
 
   function homeController($scope, data) {
   	$scope.dates = data;
-  	//debugger
+  	//$scope.mainImage = data.images[0].mainImage;
   	console.log('$scope.dates',$scope.dates);
+
+	
+	$scope.setImage = function(image, e) {
+	 // e.preventDefault();
+	  console.log(image);
+	  $scope.dates.images[0].mainImage = image;
+      //$scope.mainImage = image;
+    };
+
   }
 
 })();
